8d6767dbaa228aeea5c372a45bde1c68d25e07da
tanjoin <tanjoin@users.noreply.github.com>
2017-12-25 13:07:21 +0900

    HDR のサンプル

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # vectors
-ベクトルを表示してみるテスト
+
+ベクトルを表示してみる予定
+
+
+## Dependencies
+
 - [mrdoob/three.js: JavaScript 3D library.](https://github.com/mrdoob/three.js/)
+
+## References
+
+### HDRのサンプル
+
+- [three\.js/webgl\_hdr\.html at master · mrdoob/three\.js](https://github.com/mrdoob/three.js/blob/master/examples/webgl_hdr.html)
--- index.js
+++ index.js
@@ -1,35 +1,76 @@
+if (!Detector.webgl) {
+  Detector.addGetWebGLMessage();
+}
+
+var container, stats;
 var camera, scene, renderer;
-var geometry, material, mesh;
+var materialHDR, quad;
+var sign = 1, rate = 1;
+var clock = new THREE.Clock();
 
 init();
+
 animate();
 
 function init() {
-
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
-
+	container = document.getElementById('container');
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 900;
 	scene = new THREE.Scene();
-
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
-
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
-
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	var texture = new THREE.TextureLoader().load( "./img/IMG_6265.JPG" );
+	texture.minFilter = THREE.LinearFilter;
+	texture.magFilter = THREE.NearestFilter;
+	materialHDR = new THREE.ShaderMaterial( {
+		uniforms: {
+			tDiffuse:  { value: texture },
+			exposure:  { value: 0.125 },
+			brightMax: { value: 0.5 }
+			},
+		vertexShader: getText( 'vs-hdr' ),
+		fragmentShader: getText( 'fs-hdr' )
+	} );
+	var plane = new THREE.PlaneBufferGeometry( 512, 768 );
+	quad = new THREE.Mesh( plane, materialHDR );
+	quad.position.z = -100;
+	scene.add( quad );
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-
+	container.appendChild( renderer.domElement );
+	// stats = new Stats();
+	// container.appendChild( stats.dom );
+	//
+	window.addEventListener( 'resize', onWindowResize, false );
 }
 
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function getText( id ) {
+	return document.getElementById( id ).textContent;
+}
+
+//
+
 function animate() {
-
 	requestAnimationFrame( animate );
+	render();
+	// stats.update();
+}
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
-
+function render() {
+	var delta = clock.getDelta() * 5;
+	if ( materialHDR.uniforms.exposure.value > 0 || materialHDR.uniforms.exposure.value < 1 ) {
+		rate = 0.25;
+	} else {
+		rate = 1;
+	}
+	if ( materialHDR.uniforms.exposure.value > 5 || materialHDR.uniforms.exposure.value <= 0 ) {
+		sign *= -1;
+	}
+	materialHDR.uniforms.exposure.value += sign * rate * delta;
 	renderer.render( scene, camera );
-
 }
